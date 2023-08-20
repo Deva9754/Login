a44f45cd80f3beafbe610ac2b3e1eb9785dd39ef
Deva9754 <131431595+Deva9754@users.noreply.github.com>
2023-08-20 20:36:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login Page</title>
+</head>
+<body>
+  <h1>Login Page</h1>
+  <form id="loginForm">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br><br>
+    
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
+    
+    <button type="button" onclick="login()">Login</button>
+  </form>
+  <p id="loginMessage"></p>
+
+  <script>
+   
+  </script>
+</body>
+</html>
